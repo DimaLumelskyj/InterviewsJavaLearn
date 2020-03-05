c02f9f4167e76c3db7204ef6858b9e4a11d4b054
Dima Lumelskyj <14013882+DimaLumelskyj@users.noreply.github.com>
2020-03-05 19:21:55 +0100

    add dependency injection pattern sample

---
+++ src/main/java/pl/ordervivi/patterns/di/FullItemPropertyImpl.java
@@ -0,0 +1,8 @@
+package pl.ordervivi.patterns.di;
+
+public class FullItemPropertyImpl implements ItemProperty {
+    @Override
+    public String getSpecification() {
+        return "Full item specification";
+    }
+}
---
+++ src/main/java/pl/ordervivi/patterns/di/ItemProperty.java
@@ -0,0 +1,5 @@
+package pl.ordervivi.patterns.di;
+
+public interface ItemProperty {
+    String getSpecification();
+}
